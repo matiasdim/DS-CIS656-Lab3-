1cf5f588c09fba1c3198b32eb0b490f0662419eb
Matías <matiasdim3@gmail.com>
2017-11-07 14:18:07 -0500

    User registration done

--- LogicalClocks/src/edu/gvsu/cis/cis656/ChatClient.java
+++ LogicalClocks/src/edu/gvsu/cis/cis656/ChatClient.java
@@ -2,8 +2,11 @@ package edu.gvsu.cis.cis656;
 /*
     This is the UI thread class
  */
+import edu.gvsu.cis.cis656.clock.VectorClock;
 import edu.gvsu.cis.cis656.message.Message;
+import edu.gvsu.cis.cis656.message.MessageComparator;
 import edu.gvsu.cis.cis656.message.MessageTypes;
+import edu.gvsu.cis.cis656.queue.PriorityQueue;
 
 import java.net.DatagramSocket;
 import java.net.SocketException;
@@ -13,6 +16,9 @@ import java.net.UnknownHostException;
 public class ChatClient {
     static String HOST = "localhost";
     static int PORT = 8000;
+    int selfPid;
+    VectorClock vectorClock;
+    PriorityQueue<Message> priorityQueue;
     String username;
     DatagramSocket datagramSocket;
     InetAddress inetAddres;
@@ -22,12 +28,39 @@ public class ChatClient {
     }
 
     public void start(){
-        Message msg = new Message(MessageTypes.REGISTER, this.username, 0, null, "");
-        this.initNet();
-        Message.sendMessage(msg, this.datagramSocket, this.inetAddres, PORT);
+        this.initNetwork();
+        // Client registration
+        this.clientRegistration();
+        System.out.println("Hello " + this.username + " you entered to the chat.");
     }
 
-    private void initNet(){
+    /*
+     Auxiliar functions
+     */
+
+    // Client registration
+    private void clientRegistration(){
+        Message response = this.postMessage(MessageTypes.REGISTER, this.username, 0, null, "");
+        if(response.type == MessageTypes.ACK){
+            this.selfPid = response.pid;
+            vectorClock = new VectorClock();
+            vectorClock.addProcess(this.selfPid, 0);
+            this.priorityQueue = new PriorityQueue<>(new MessageComparator());
+        }else if (response.type == MessageTypes.ERROR){
+            System.out.println(response.message);
+            System.exit(1);
+        }
+    }
+
+    //Send mesage
+    private Message postMessage(int type, String username, int pid, VectorClock ts, String message){
+        Message msg = new Message(MessageTypes.REGISTER, this.username, 0, null, "");
+        Message.sendMessage(msg, this.datagramSocket, this.inetAddres, PORT);
+        return Message.receiveMessage(this.datagramSocket);
+    }
+
+    //Network config
+    private void initNetwork(){
         try {
             this.datagramSocket = new DatagramSocket();
             this.inetAddres = InetAddress.getByName(HOST);
